1e5c5c875fe33ad13944f541a9e786943f131abe
pradipta <pradiptananda.dalai@gmail.com>
2021-03-21 21:52:17 +0530

    testing

---
+++ src/main/java/com/example/dockerjenkin/DockerJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerjenkin/DockerJenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
